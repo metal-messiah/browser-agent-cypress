4bb05de02ed016b26a32075737067f9f28313836
Jordan Porter <silentsummitsoftware@gmail.com>
2021-05-11 14:54:34 -0700

    check known tests

--- cypress/integration/jil-server.test.js
+++ cypress/integration/jil-server.test.js
@@ -5,20 +5,12 @@ describe('My First Test', () => {
         cy.visit('http://bam-test-1.nr-local.net:3333/')
       })
 
-      it('works on a known test that fails with cypress', () => {
-        cy.get('a').contains('tests/browser/spa/cancelled-timer-twice.test.js').each(e => {
-            cy.log(e[0].innerText)
-            cy.visit(e[0].href)
-            cy.contains('ok 1 interaction should be null at first').should('be.visible')
-            cy.contains('ok 2 first timer fired').should('be.visible')
+    it('navigates to all tests with XHR and known failing tests', () => {
+        cy.get('a').each(e => {
+            if (e[0].innerText.toLowerCase().includes('xhr') || e[0].innerText.toLowerCase().includes('timer')){
+              cy.log(e[0].innerText)
+              cy.visit(e[0].href, {timeout: 30000}) // waits for full page load
+            }
         })
-      })
-
-    // it('navigates to all tests in the list', () => {
-    //     cy.get('a').each(e => {
-    //         cy.log(e[0].innerText)
-    //         cy.visit(e[0].href, {timeout: 30000})
-    //         cy.contains('TAP', {timeout: 30000}).should('be.visible')
-    //     })
-    // })
+    })
   })
